e4992455bab6f7f8b399fffc8e519ca2ac084961
astitt1 <astitt@perscholas.org>
2024-03-11 13:50:16 -0400

    added post route for users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
 
 const users = require("./data/users");
 const posts = require("./data/posts");
@@ -11,6 +12,11 @@ const posts = require("./data/posts");
 //   res.status(404).json({ error: `Resource not found` });
 // });
 
+//MIDDLEWARE
+//BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Work in progress");
 });
@@ -19,15 +25,41 @@ app.get("/", (req, res) => {
 app.get("/api/users", (req, res) => {
   res.json(users);
 });
-// POST USER
+// POST USER - create a new user
+
+app.post("/api/users", (req, res) => {
+  // Within the POST request route, we create a new
+  // user with the data given by the client.
+  // We should also do some more robust validation here,
+  // but this is just an example for now.
+  if (req.body.name && req.body.username && req.body.email) {
+    if (users.find((u) => u.username == req.body.username)) {
+      res.json({ error: `Username already taken` });
+      return;
+    }
+
+    const newUser = {
+      id: users[users.length - 1].id + 1,
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+    };
+
+    users.push(newUser);
+    res.json(users[users.length - 1]);
+  } else {
+    res.json({ error: "Insufficient Data" });
+  }
+});
+
 // GET USER by id
 app.get("/api/users/:id", (req, res, next) => {
   try {
     const user = users.find((u) => u.id == req.params.id);
     if (user) res.json(user);
-    next()
+    next();
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 });
 // PATCH/PUT USER by id
@@ -47,9 +79,15 @@ app.get("/api/posts/:id", (req, res, next) => {
 
 //Lesson error handling middleware
 app.use((req, res) => {
-    res.status(404);
-    res.json({ error: `Resource not found` });
-  });
+  res.status(404);
+  res.json({ error: `Resource not found` });
+});
+
+// //The code above written as a catch-all route
+// app.get('/*', (req, res)=>{
+//     res.status(404);
+//     res.json({ error: `Resource not found` });
+// })
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
